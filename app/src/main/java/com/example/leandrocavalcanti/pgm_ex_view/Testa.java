403fa3fc036f04e8ecfdbc577a2fe89b17fdaf45
leandrooliveirads <leandro_oliveira321@hotmail.com>
2017-10-04 12:19:53 -0300

    correção API

---
+++ app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/Testa.java
@@ -0,0 +1,13 @@
+package com.example.leandrocavalcanti.pgm_ex_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Testa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testa);
+    }
+}
