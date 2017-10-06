3674f4b4dda2bb21b87e9836bda80d01b850f135
leandrooliveirads <leandro_oliveira321@hotmail.com>
2017-10-06 12:44:15 -0300

    tela 2 incompleta

--- app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/activity/MainActivity.java
+++ app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/activity/MainActivity.java
@@ -1,28 +1,32 @@
 package com.example.leandrocavalcanti.pgm_ex_view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.example.leandrocavalcanti.pgm_ex_view.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btnNextPage1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNextPage1 = (Button) findViewById(R.id.btnNextPage1);
+        btnNextPage1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnNextPage1:
-//                Intent i = new Intent(MainActivity.this, Tela2Activity.class);
-//                startActivity(i);
-                Toast.makeText(this, "skfaklsfaskl", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, Tela2Activity.class);
+                startActivity(i);
                 break;
 
         }
