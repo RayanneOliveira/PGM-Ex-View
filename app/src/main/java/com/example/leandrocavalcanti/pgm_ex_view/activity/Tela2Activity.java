01f05b18f459dec0a95cf86294a597a0cd017fd3
leandrooliveirads <leandro_oliveira321@hotmail.com>
2017-10-09 12:04:12 -0300

    tela 2 incompleta

--- app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/activity/Tela2Activity.java
+++ app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/activity/Tela2Activity.java
@@ -1,15 +1,73 @@
 package com.example.leandrocavalcanti.pgm_ex_view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.leandrocavalcanti.pgm_ex_view.R;
+import com.example.leandrocavalcanti.pgm_ex_view.domain.Jogador;
 
-public class Tela2Activity extends AppCompatActivity {
+public class Tela2Activity extends AppCompatActivity implements View.OnClickListener{
+
+    private Jogador jogador;
+    private CheckBox chkFalta;
+    private CheckBox chkPenalty;
+    private Switch switchTitular;
+    private Button tela2BtnPrint;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+
+        pegarReferencias();
+        listenar();
+
     }
+
+    private void listenar() {
+        tela2BtnPrint.setOnClickListener(this);
+    }
+
+    private void pegarReferencias() {
+        chkFalta = (CheckBox) findViewById(R.id.checkBoxFalta);
+        chkPenalty = (CheckBox) findViewById(R.id.checkBoxPenalty);
+        switchTitular = (Switch) findViewById(R.id.switchTitular);
+        tela2BtnPrint = (Button) findViewById(R.id.tela2BtnPrint);
+        seekBar = (SeekBar) findViewById(R.id.seekBarSpeed);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.tela2BtnPrint:
+                try {
+                    imprimirJogador();
+                } catch (Exception e) {
+                    Toast.makeText(this, "Erro: \n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void imprimirJogador() throws Exception {
+        jogador = new Jogador();
+        jogador.setNome("Neymar");
+        jogador.setVelocidade(seekBar.getProgress());
+
+        Toast.makeText(this, jogador.getNome() + "\nVelocidade: \n" + jogador.getVelocidade()
+        ,Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/domain/Jogador.java
+++ app/src/main/java/com/example/leandrocavalcanti/pgm_ex_view/domain/Jogador.java
@@ -5,4 +5,150 @@ package com.example.leandrocavalcanti.pgm_ex_view.domain;
  */
 
 public class Jogador {
+
+    private Boolean isContratado;
+    private Boolean isBatedorFalta;
+    private Boolean isBatedorPenalty;
+    private Boolean isTitular;
+    private Integer camisa;
+    private Boolean isAquecido;
+    private Integer velocidade;
+    private String corChuteira;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getContratado() {
+        return isContratado;
+    }
+
+    public void setContratado(Boolean contratado) {
+        isContratado = contratado;
+    }
+
+    public Boolean getBatedorFalta() {
+        return isBatedorFalta;
+    }
+
+    public void setBatedorFalta(Boolean batedorFalta) {
+        isBatedorFalta = batedorFalta;
+    }
+
+    public Boolean getBatedorPenalty() {
+        return isBatedorPenalty;
+    }
+
+    public void setBatedorPenalty(Boolean batedorPenalty) {
+        isBatedorPenalty = batedorPenalty;
+    }
+
+    public Boolean getTitular() {
+        return isTitular;
+    }
+
+    public void setTitular(Boolean titular) {
+        isTitular = titular;
+    }
+
+    public Integer getCamisa() {
+        return camisa;
+    }
+
+    public void setCamisa(Integer camisa) {
+        this.camisa = camisa;
+    }
+
+    public Boolean getAquecido() {
+        return isAquecido;
+    }
+
+    public void setAquecido(Boolean aquecido) {
+        isAquecido = aquecido;
+    }
+
+    public Integer getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(Integer velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public String getCorChuteira() {
+        return corChuteira;
+    }
+
+    public void setCorChuteira(String corChuteira) {
+        this.corChuteira = corChuteira;
+    }
+
+    public Jogador() {
+    }
+
+
+
+    public Jogador(Boolean isContratado, Boolean isBatedorFalta, Boolean isBatedorPenalty, Boolean isTitular, Integer camisa, Boolean isAquecido, Integer velocidade, String corChuteira) {
+        this.isContratado = isContratado;
+        this.isBatedorFalta = isBatedorFalta;
+        this.isBatedorPenalty = isBatedorPenalty;
+        this.isTitular = isTitular;
+        this.camisa = camisa;
+        this.isAquecido = isAquecido;
+        this.velocidade = velocidade;
+        this.corChuteira = corChuteira;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Jogador jogador = (Jogador) o;
+
+        if (isContratado != null ? !isContratado.equals(jogador.isContratado) : jogador.isContratado != null)
+            return false;
+        if (isBatedorFalta != null ? !isBatedorFalta.equals(jogador.isBatedorFalta) : jogador.isBatedorFalta != null)
+            return false;
+        if (isBatedorPenalty != null ? !isBatedorPenalty.equals(jogador.isBatedorPenalty) : jogador.isBatedorPenalty != null)
+            return false;
+        if (isTitular != null ? !isTitular.equals(jogador.isTitular) : jogador.isTitular != null)
+            return false;
+        if (getCamisa() != null ? !getCamisa().equals(jogador.getCamisa()) : jogador.getCamisa() != null)
+            return false;
+        if (isAquecido != null ? !isAquecido.equals(jogador.isAquecido) : jogador.isAquecido != null)
+            return false;
+        if (getVelocidade() != null ? !getVelocidade().equals(jogador.getVelocidade()) : jogador.getVelocidade() != null)
+            return false;
+        if (getCorChuteira() != null ? !getCorChuteira().equals(jogador.getCorChuteira()) : jogador.getCorChuteira() != null)
+            return false;
+        return getNome() != null ? getNome().equals(jogador.getNome()) : jogador.getNome() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = isContratado != null ? isContratado.hashCode() : 0;
+        result = 31 * result + (isBatedorFalta != null ? isBatedorFalta.hashCode() : 0);
+        result = 31 * result + (isBatedorPenalty != null ? isBatedorPenalty.hashCode() : 0);
+        result = 31 * result + (isTitular != null ? isTitular.hashCode() : 0);
+        result = 31 * result + (getCamisa() != null ? getCamisa().hashCode() : 0);
+        result = 31 * result + (isAquecido != null ? isAquecido.hashCode() : 0);
+        result = 31 * result + (getVelocidade() != null ? getVelocidade().hashCode() : 0);
+        result = 31 * result + (getCorChuteira() != null ? getCorChuteira().hashCode() : 0);
+        result = 31 * result + (getNome() != null ? getNome().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
